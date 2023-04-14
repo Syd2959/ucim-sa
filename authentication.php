b92a28e608fa5ab75cdb87dd9e7db4a05c39f235
Syd2959 <130773437+Syd2959@users.noreply.github.com>
2023-04-14 16:53:51 +0200

    Create authentication.php

---
+++ authentication.php
@@ -0,0 +1,22 @@
+<?php   
+
+    //if ($_SERVER["REQUEST_METHOD"] == "POST") { // zistíme, či bola požiadavka odoslaná metódou POST
+
+        $username = isset($_POST['username']) ? $_POST['username'] : 'hovno';
+        $loginpassword = isset($_POST['password']) ? $_POST['password'] : 'hovno';
+       
+        echo $username . " " . $loginpassword . "<br>";
+
+        // Porovnanie s uloženým používateľským menom a heslom (tu môžete pridať svoju vlastnú logiku autentifikácie)
+        if ($username == 'jakub' && $loginpassword == 'jakub') {
+        // Ak sú používateľské meno a heslo správne, nastavíme premennú $_SESSION['user'] s hodnotou používateľského mena
+        $_SESSION['user'] = $username;
+        echo 'success'; // Odpoveď pre úspešnú autentifikáciu
+        } else {
+        echo 'Nesprávne používateľské meno alebo heslo.'; // Odpoveď pre neúspešnú autentifikáciu
+        }
+//} else {
+    //echo 'Neplatný požiadavok.'; // Odpoveď pre neplatnú požiadavku
+//}
+       
+?>
